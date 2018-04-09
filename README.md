947c4ad3750ba75f048d42d6c75d3cc4d521da11
suhaboncukcu <suha.boncukcu@portera.nl>
2018-04-09 15:01:25 +0300

    initial commit

---
+++ README.md
@@ -0,0 +1,7 @@
+1 - clone the repository
+2 - npm install
+3 - npm run watch
+4 - check the index.html and use app.css and app.js wherever you like as you see. 
+
+That's it!
+
---
+++ assets/app/components/App.vue
@@ -0,0 +1,17 @@
+<template>
+	<section id="app" class="section hero is-fullheight is-primary is-bold">
+		<div class="hero-body">
+            <div class="container">
+                <div class="title">
+                    <p class="has-text-dark">looking</p>
+                    <p class="is-size-1">delicious.</p>
+                </div>
+            </div>
+        </div>
+	</section>
+</template>
+
+<script>
+	export default {
+	}
+</script>
